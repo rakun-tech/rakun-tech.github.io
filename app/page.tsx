448b5649650885d0fa4642224ec18f32c943c1f2
Uğur Aldanmaz <ugur.aldanmaz@outlook.com>
2025-02-25 06:19:45 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function UnderConstruction() {
         <div className="flex items-center justify-center min-h-screen bg-[#2C3E50]">
             <div className="text-center inline-block align-top">
                 <h1 className="text-5xl font-bold mb-4">Under Construction</h1>
-                <p className="text-xl text-[#F8F9FA] mb-8">We're working hard to bring you something amazing. Stay tuned!</p>
+                <p className="text-xl text-[#F8F9FA] mb-8">We are working hard to bring you something amazing. Stay tuned!</p>
 
                 <p className="text-[#D5D8DC]">© 2025 Stealth Realm. All rights reserved.</p>
             </div>
